642cdbe2eeb885726ada5050478795e9f5e6196b
chibez <odobicaleb@gmail.com>
2024-10-26 06:35:23 +0000

    Unordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python3
 """
+Markdown to HTML converter script
+"""
+
+import sys
+import os
+
+if __name__ == '__main__':
+    # Check if the number of arguments is less than 2
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    # Assign arguments to variables
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    # If all checks pass, exit with status 0
+    sys.exit(0)
+
+
+
+#!/usr/bin/python3
+"""
 Markdown to HTML converter script that parses headings.
 """
 
